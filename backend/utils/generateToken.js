6083bd33437a8d0ed5d1611435422fd952698962
moizayub1255 <moizayub401@gmail.com>
2026-02-14 21:50:02 +0500

    cors issue

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,16 +2,14 @@ import jwt from "jsonwebtoken";
 import { ENV_VARS } from "../config/envVars.js";
 
 export const generateTokenAndSetCookie = (userId, res) => {
-    const token = jwt.sign({userId}, ENV_VARS.JWT_SECRET, {expiresIn: "15d"});
+  const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
 
-    
-    res.cookie("jwt-netflix", token, {
-        maxAge: 15 * 24 * 60 * 60 * 1000, // 15 Days in milliseconds
-        httpOnly: true, // prevent XSS attacks cross-site scripting attacks, make it not accessible by JS
-        sameSite: "strict", // CSRF attacks cross-site request forgery attacks 
-        secure: ENV_VARS.NODE_ENV !== "development", 
+  res.cookie("jwt-netflix", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 Days in milliseconds
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks, make it not accessible by JS
+    sameSite: "none", // Allow cross-site cookie transmission
+    secure: true, // Required when sameSite is "none"
+  });
 
-    });
-
-    return token;
+  return token;
 };
